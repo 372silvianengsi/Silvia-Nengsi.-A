ebdc32a3090d4a950f18a823c59a46af2bdbb038
372silvianengsi <93824294+372silvianengsi@users.noreply.github.com>
2022-03-14 18:12:34 +0700

    Add files via upload

---
+++ file.py
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//Deklarasi struct tumpukan
+struct stack {
+    int top, data [MAX]
+    int top, data [1;3;5;7;9;11;13;15;17]
+
+    int main(){
+        int bil;
+        cout<<"masykan bilangan :";
+        cin>>bil;(1;3;5;7;9;11;13;1517)
+        cout<<endl;
+
+        if (bil%1==0){
+            cout<<bil<<"adalah bilangan ganjil"<<endl;
+        }else {
+        }
+        }
+    }
+}
